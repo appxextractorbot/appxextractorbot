03074af51e05abe234920d1c580b0b3e30975b7f
appxextractorbot <appxextractorbot@gmail.com>
2025-04-29 16:03:55 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "27058143"
 api_hash = "c569ea4f5ade3fc70d2bb1fd162d9dc9"
-bot_token = "8042787574:AAEjO7dhYFCTlhhrKgVbWS-apVYn-5hG_cQ"
+bot_token = "8151353387:AAEOH7rcRZg2JKSgfrOaN1kbE3d-ffgGLTo"
 auth_users = [8144269730]
